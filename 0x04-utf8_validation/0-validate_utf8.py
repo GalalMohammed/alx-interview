db89e0e88373e7119d0da5ec535cddd36cd9698e
GalalMohammed <galalmoh2001@gmail.com>
2024-01-22 11:14:36 +0200

    add task 0

---
+++ 0x04-utf8_validation/0-validate_utf8.py
@@ -0,0 +1,31 @@
+#!/usr/bin/python3
+# -*- coding: utf-8 -*-
+"""
+This module is about UTF-8 Validation.
+
+Example:
+    print(validUTF8(data))
+
+"""
+
+
+def validUTF8(data):
+    """Determine if a given data set represents a valid UTF-8 encoding.
+
+    Args:
+        data (list): list of characters.
+
+    Returns:
+        True if data is a valid UTF-8 encoding, else return False.
+
+    """
+    i = 0
+    while i < len(data):
+        data_byte = data[i] & 255
+        while data_byte & 128:
+            i += 1
+            if not ((data[i] >> 6) & 3) == 2:
+                return False
+            data_byte <<= 1
+        i += 1
+    return True
