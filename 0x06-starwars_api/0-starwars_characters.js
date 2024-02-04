26dcc6a1efe53aa84ec6d27ddf19f00b19b428c3
GalalMohammed <galalmoh2001@gmail.com>
2024-02-05 00:38:35 +0200

    workaround to await responses

--- 0x06-starwars_api/0-starwars_characters.js
+++ 0x06-starwars_api/0-starwars_characters.js
@@ -3,9 +3,13 @@ const request = require('request');
 
 // Send a GET request to Star wars API
 request.get('https://swapi-api.alx-tools.com/api/films/' + process.argv[2], (_err, _response, body) => {
-  for (const character of JSON.parse(body).characters) {
-    request.get(character, (_err, _response, body) => {
+  printChar(JSON.parse(body).characters, 0);
+});
+const printChar = (characters, idx) => {
+  if (idx < characters.length) {
+    request.get(characters[idx], (_err, _response, body) => {
       console.log(JSON.parse(body).name);
+      printChar(characters, idx + 1);
     });
   }
-});
+};
